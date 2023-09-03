088dce7fcd587b0fae3054b8c6e30cca76be0871
Evangelos <contact@vagdedes.com>
2023-09-04 00:25:50 +0300

    Improved object expectations

--- library/account/api/objects/communication/AccountCooperation.php
+++ library/account/api/objects/communication/AccountCooperation.php
@@ -2,7 +2,7 @@
 
 class AccountCooperation
 {
-    private ?Account $account;
+    private Account $account;
     //todo introduce constants that are null application-id
 
     public function __construct($account)
