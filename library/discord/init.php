907816690ad9cfd4ebcb524ad7d92ff4ad957a42
Evangelos <contact@vagdedes.com>
2023-10-09 10:34:44 +0300

    package correction

--- library/discord/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '/var/www/.structure/library/discord/api/connect.php';
-require_once '/var/www/.structure/library/discord/api/variables.php';
-
-require_once '/var/www/.structure/library/discord/api/handlers/webhook/base.php';
-require_once '/var/www/.structure/library/discord/api/handlers/webhook/executors.php';
