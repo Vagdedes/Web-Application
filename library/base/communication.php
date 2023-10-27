f34333007453be936b854e560542050e2b14bfc3
Evangelos <contact@vagdedes.com>
2023-10-27 17:00:05 +0300

    Private communication improvements

--- library/base/communication.php
+++ library/base/communication.php
@@ -4,6 +4,7 @@ require_once '/var/www/.structure/library/base/requirements/sql_connection.php';
 require_once '/var/www/.structure/library/memory/init.php';
 $administrator_local_server_ip_addresses_table = "administrator.localServerIpAddresses";
 $memory_private_connections_table = "memory.privateConnections";
+$private_connection_access = false;
 
 function private_file_get_contents($url, $createAndClose = false): bool|string
 {
@@ -34,72 +35,83 @@ function private_file_get_contents($url, $createAndClose = false): bool|string
 
 function is_private_connection($checkClientIP = false): bool
 {
-    global $memory_private_connections_table;
+    global $private_connection_access;
 
-    if (isset($_POST['private_verification_key'])) {
-        load_sql_database(SqlDatabaseCredentials::MEMORY);
-        $query = get_sql_query( // No cache required, already in memory table
-            $memory_private_connections_table,
-            array("id"),
-            array(
-                array("code", hash("sha512", $_POST['private_verification_key']))
-            ),
-            null,
-            1
-        );
+    if ($private_connection_access) {
+        return true;
+    } else {
+        global $memory_private_connections_table;
 
-        if (!empty($query)
-            && !has_memory_cooldown(__METHOD__, null, false)) {
+        if (isset($_POST['private_verification_key'])) {
             global $administrator_local_server_ip_addresses_table;
-            delete_sql_query(
+            load_sql_database(SqlDatabaseCredentials::MEMORY);
+            $query = get_sql_query( // No cache required, already in memory table
                 $memory_private_connections_table,
-                array(
-                    array("id", "=", $query[0]->id, 0),
-                    array("expiration", "<", time())
-                )
-            );
-            load_previous_sql_database();
-            set_sql_cache();
-            if (!empty(get_sql_query(
-                $administrator_local_server_ip_addresses_table,
                 array("id"),
                 array(
-                    array("ip_address", get_local_ip_address()),
-                    array("deletion_date", null),
-                    null,
-                    array("expiration_date", "IS", null, 0),
-                    array("expiration_date", ">", get_current_date()),
-                    null,
+                    array("code", hash("sha512", $_POST['private_verification_key']))
                 ),
                 null,
                 1
-            ))) {
-                if ($checkClientIP) {
-                    set_sql_cache();
-                    return !empty(get_sql_query(
-                        $administrator_local_server_ip_addresses_table,
-                        array("id"),
-                        array(
-                            array("ip_address", get_raw_client_ip_address()),
-                            array("deletion_date", null),
-                            null,
-                            array("expiration_date", "IS", null, 0),
-                            array("expiration_date", ">", get_current_date()),
-                            null,
-                        ),
+            );
+            $cacheKey = array(__METHOD__, get_raw_client_ip_address());
+
+            if (!empty($query)
+                && !has_memory_cooldown($cacheKey, null, false)) {
+                delete_sql_query(
+                    $memory_private_connections_table,
+                    array(
+                        array("id", "=", $query[0]->id, 0),
+                        array("expiration", "<", time())
+                    )
+                );
+                load_previous_sql_database();
+                set_sql_cache();
+                if (!empty(get_sql_query(
+                    $administrator_local_server_ip_addresses_table,
+                    array("id"),
+                    array(
+                        array("ip_address", get_local_ip_address()),
+                        array("deletion_date", null),
                         null,
-                        1
-                    ));
-                } else {
-                    return true;
+                        array("expiration_date", "IS", null, 0),
+                        array("expiration_date", ">", get_current_date()),
+                        null,
+                    ),
+                    null,
+                    1
+                ))) {
+                    if ($checkClientIP) {
+                        set_sql_cache();
+                        if (!empty(get_sql_query(
+                            $administrator_local_server_ip_addresses_table,
+                            array("id"),
+                            array(
+                                array("ip_address", get_raw_client_ip_address()),
+                                array("deletion_date", null),
+                                null,
+                                array("expiration_date", "IS", null, 0),
+                                array("expiration_date", ">", get_current_date()),
+                                null,
+                            ),
+                            null,
+                            1
+                        ))) {
+                            $private_connection_access = true;
+                            return true;
+                        }
+                    } else {
+                        $private_connection_access = true;
+                        return true;
+                    }
                 }
+            } else {
+                has_memory_cooldown($cacheKey, "5 minutes", true, true);
+                load_previous_sql_database();
             }
-        } else {
-            has_memory_cooldown(__METHOD__, "5 minutes", true, true);
-            load_previous_sql_database();
         }
+        return false;
     }
-    return false;
 }
 
 function get_private_ip_address(): ?string
