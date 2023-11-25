65c2ad4e95dc4324f300c5d0a1426c0270ca7fac
Evangelos <contact@vagdedes.com>
2023-11-25 21:03:03 +0200

    Account objectives can now be temporary

--- library/account/api/objects/account/AccountObjectives.php
+++ library/account/api/objects/account/AccountObjectives.php
@@ -13,11 +13,10 @@ class AccountObjectives
     public function get(): array
     {
         if (!$this->account->getEmail()->isVerified()) {
-            return array(
-                $this->create(
-                    "Email Verification",
-                    "Verify your email by clicking the verification link we have emailed you.",
-                )
+            return $this->create(
+                array(),
+                "Email Verification",
+                "Verify your email by clicking the verification link we have emailed you.",
             );
         } else {
             global $website_account_url;
@@ -26,59 +25,74 @@ class AccountObjectives
             $paypal = $this->account->getAccounts()->hasAdded(AccountAccounts::PAYPAL_EMAIL, null, 1)->isPositiveOutcome();
 
             if (!$this->account->getAccounts()->hasAdded(AccountAccounts::STRIPE_EMAIL, null, 1)->isPositiveOutcome()) {
-                $array[] = $this->create(
+                $array = $this->create(
+                    $array,
                     "Purchases & Transactions",
                     "Add your" . (!$paypal ? " PayPal and " : " ") . "Stripe email to have your purchases identified.",
                     $website_account_url . "/profile/addAccount",
-                    true
+                    true,
+                    "14 days"
                 );
             } else if (!$paypal) {
-                $array[] = $this->create(
+                $array = $this->create(
+                    $array,
                     "Purchases & Transactions",
                     "Add your PayPal email to have your purchases identified.",
                     $website_account_url . "/profile/addAccount",
-                    true
+                    true,
+                    "14 days"
                 );
             }
             if (!$this->account->getAccounts()->hasAdded(AccountAccounts::SPIGOTMC_URL, null, 1)->isPositiveOutcome()
                 && !$this->account->getAccounts()->hasAdded(AccountAccounts::BUILTBYBIT_URL, null, 1)->isPositiveOutcome()
                 && !$this->account->getAccounts()->hasAdded(AccountAccounts::POLYMART_URL, null, 1)->isPositiveOutcome()) {
-                $array[] = $this->create( // Do not mention SpigotMC, it's automatically found
+                $array = $this->create( // Do not mention SpigotMC, it's automatically found
+                    $array,
                     "Minecraft Platform",
                     "Add your SpigotMC/BuiltByBit/Polymart account URL to have your licenses identified.",
                     $website_account_url . "/profile/addAccount",
-                    true
+                    true,
+                    "14 days"
                 );
             }
             if (!$this->account->getAccounts()->hasAdded(AccountAccounts::DISCORD_TAG, null, 1)->isPositiveOutcome()) {
-                $array[] = $this->create(
+                $array = $this->create(
+                    $array,
                     "Discord Tag",
                     "Add your Discord-Tag so we can give you roles on Discord now or in the future.",
                     $website_account_url . "/profile/addAccount",
-                    true
+                    true,
+                    "14 days"
                 );
             }
             if (!$this->account->getAccounts()->hasAdded(AccountAccounts::PATREON_FULL_NAME, null, 1)->isPositiveOutcome()) {
-                $array[] = $this->create(
+                $array = $this->create(
+                    $array,
                     "Patreon Full Name",
                     "Add your Patreon-Full-Name to have your purchases identified.",
                     $website_account_url . "/profile/addAccount",
-                    true
+                    true,
+                    "14 days"
                 );
             }
             return $array;
         }
     }
 
-    private function create(string  $title, string $description,
-                            ?string $url = null, bool $optionalURL = false): object
+    private function create(array   $array,
+                            string  $title, string $description,
+                            ?string $url = null, bool $optionalURL = false,
+                            ?string $duration = null): array
     {
-        $object = new stdClass();
-        $object->title = $title;
-        $object->description = $description;
-        $object->url = $url;
-        $object->optional_url = $optionalURL;
-        return $object;
+        if ($duration === null || get_past_date($duration) <= $this->account->getDetail("creation_date")) {
+            $object = new stdClass();
+            $object->title = $title;
+            $object->description = $description;
+            $object->url = $url;
+            $object->optional_url = $optionalURL;
+            $array[] = $object;
+        }
+        return $array;
     }
 
     public function has(): bool
