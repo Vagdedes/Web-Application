cf96ae1744e6836198e1fa23ee4e1d56d8a0b8ab
Evangelos <contact@vagdedes.com>
2023-12-28 16:04:55 +0200

    Implemented replacement base library

--- library/base/placeholder.php
+++ library/base/placeholder.php
@@ -5,30 +5,58 @@ class InformationPlaceholder
 
     public const
         STARTER = "%%__",
-        DIVISOR = "_",
+        DIVISOR = " ",
+        DIVISOR_REPLACEMENT = "_",
         ENDER = "__%%";
 
-    public function __construct()
+    private string $starter, $divisor, $divisorReplacement, $ender;
+
+    public function __construct(string $starter = null,
+                                string $divisor = null,
+                                string $divisorReplacement = null,
+                                string $ender = null)
     {
+        $this->starter = $starter !== null ? $starter : self::STARTER;
+        $this->divisor = $divisor !== null ? $divisor : self::DIVISOR;
+        $this->divisorReplacement = $divisorReplacement !== null ? $divisorReplacement : self::DIVISOR_REPLACEMENT;
+        $this->ender = $ender !== null ? $ender : self::ENDER;
     }
 
-    public function replace(?string $string, array $objects): ?string
+    public function replace(string|array|object|null $input, array|object|null $replacements): string|array|object|null
     {
-        if ($string !== null) {
-            foreach ($objects as $class => $object) {
-                try {
-                    foreach ($object as $key => $value) {
-                        $string = str_replace($this->build($class, $key), $value, $string);
-                    }
-                } catch (Exception $ignored) {
-                }
+        if (is_array($input)) {
+            foreach ($input as $key => $value) {
+                $input[$key] = $this->build($value, $replacements);
             }
+            return $input;
+        } else if (is_object($input)) {
+            $object = new stdClass();
+
+            foreach (json_decode(json_encode($input), true) as $key => $value) {
+                $object->{$key} = $this->build($value, $replacements);
+            }
+            return $object;
+        } else {
+            return $this->build($input, $replacements);
         }
-        return $string;
     }
 
-    public function build(string $class, mixed $property): string
+    private function build(?string $input, array|object|null $replacements): ?string
     {
-        return self::STARTER . $class . self::DIVISOR . $property . self::ENDER;
+        if ($input === null || empty($replacements)) {
+            return $input;
+        } else {
+            if (is_object($replacements)) {
+                $replacements = json_decode(json_encode($replacements), true);
+            }
+            foreach ($replacements as $current => $replacement) {
+                $input = str_replace(
+                    $this->starter . str_replace($this->divisor, $this->divisorReplacement, $current) . $this->ender,
+                    $replacement,
+                    $input
+                );
+            }
+            return $input;
+        }
     }
 }
