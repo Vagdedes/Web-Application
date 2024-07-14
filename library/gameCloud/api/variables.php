4e1eed3d917bc28e8282f7818c17ef9a781cd4e2
Evangelos <contact@vagdedes.com>
2024-07-14 22:19:40 +0300

    Removed the cross server information functionality

--- library/gameCloud/api/variables.php
+++ library/gameCloud/api/variables.php
@@ -4,7 +4,6 @@ $punished_players_table = "gameCloud.punishedPlayers";
 $configuration_changes_table = "gameCloud.automaticConfigurationChanges";
 $disabled_detections_table = "gameCloud.disabledDetections";
 $connection_count_table = "gameCloud.connectionCount";
-$cross_server_information_table = "gameCloud.crossServerInformation";
 $accepted_purposes_table = "gameCloud.acceptedPurposes";
 $failed_discord_webhooks_table = "gameCloud.failedDiscordWebhooks";
 $accepted_platforms_table = "gameCloud.acceptedPlatforms";
