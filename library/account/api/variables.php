7a5f3c5272d0e4b2fcc63a3f759478cbbd79ff42
Evangelos <vaggdedes@gmail.com>
2024-09-28 09:22:10 +0300

    Update variables.php

--- library/account/api/variables.php
+++ library/account/api/variables.php
@@ -4,22 +4,10 @@ if (!isset($administrator_local_server_ip_addresses_table)) { // Already set by 
     $administrator_local_server_ip_addresses_table = "administrator.localServerIpAddresses";
 }
 $administrator_public_server_ip_addresses_table = "administrator.publicServerIpAddresses";
-$administrator_application_api_connections_table = "administrator.applicationApiConnections";
-$administrator_application_api_keys_table = "administrator.applicationApiKeys";
-$administrator_application_api_paths_table = "administrator.applicationApiPaths";
-$administrator_application_api_extra_table = "administrator.applicationExtraInformation";
 
 // Session
 $account_sessions_table = "session.sessions";
 $instant_logins_table = "session.instantLogins";
-
-// Translation
-$translation_languages_table = "translation.languages";
-$translation_text_table = "translation.text";
-
-// Knowledge
-$knowledge_types_table = "knowledge.types";
-$knowledge_information_table = "knowledge.information";
 
 // Account
 $accounts_table = "account.accounts";
@@ -69,43 +57,14 @@ $giveaway_winners_table = "giveaway.winners";
 // Transactions
 $unknown_email_processing_table = "transactions.unknownEmailProcessing";
 
-// Communication
-$communication_contents_table = "communication.contents";
-$communication_invitations_table = "communication.invitations";
-$communication_threads_table = "communication.threads";
-$communication_types_table = "communication.types";
-
-// Cooperation
-$cooperation_votings_table = "cooperation.votings";
-$cooperation_invitations_table = "cooperation.invitations";
-$cooperation_choices_table = "cooperation.choices";
-$cooperation_selections_table = "cooperation.selections";
-
 // Correlation
 $added_accounts_table = "correlation.addedAccounts";
 $accepted_accounts_table = "correlation.accounts";
-$correlation_instants_table = "correlation.instant";
-$correlation_requests_table = "correlation.requests";
-$correlation_types_table = "correlation.types";
 
 // Tickets
 $tickets_email_table = "tickets.email";
 
-// Files
-$file_files_table = "file.files";
-$file_sharing_table = "file.sharing";
-$file_types_table = "file.types";
-$file_downloads_table = "file.downloads";
-
-// Team
-$team_invitations_table = "team.invitations";
-$team_modifications_table = "team.modifications";
-$team_teams_table = "team.teams";
-
 // Verification
-$verification_instant_table = "verification.instant";
-$verification_password_table = "verification.password";
-$verification_types_table = "verification.types";
 $email_verifications_table = "verification.emailVerifications";
 $change_password_table = "verification.changePassword";
 
