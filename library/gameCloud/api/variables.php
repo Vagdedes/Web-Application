c2251cbb42cc5045aae445fde3063c0bb571bfb8
Evangelos <vaggdedes@gmail.com>
2024-10-16 20:25:52 +0300

    Removed the Detection Slots

--- library/gameCloud/api/variables.php
+++ library/gameCloud/api/variables.php
@@ -8,12 +8,3 @@ $accepted_purposes_table = "gameCloud.acceptedPurposes";
 $failed_discord_webhooks_table = "gameCloud.failedDiscordWebhooks";
 $accepted_platforms_table = "gameCloud.acceptedPlatforms";
 $staff_announcements_table = "gameCloud.staffAnnouncements";
-$detection_slots_table = "gameCloud.detectionSlots";
-$detection_slots_tracking_table = "gameCloud.detectionSlotsTracking";
-
-class GameCloudVariables
-{
-    public const
-        SPARTAN_SYN = 7,
-        DETECTION_SLOTS_UNLIMITED_PRODUCT = 26;
-}
