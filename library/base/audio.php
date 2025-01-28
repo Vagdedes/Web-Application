f3cd0b908e7d4de0001758ad038561b5ca7ecd92
Evangelos <vaggdedes@gmail.com>
2025-01-28 04:45:32 +0200

    Create audio.php

---
+++ library/base/audio.php
@@ -0,0 +1,59 @@
+<?php
+
+function convert_base64_ogg_to_base64_mp3(string $base64): ?string
+{
+    // 1.
+    $oldData = @base64_decode($base64);
+
+    if ($oldData === false) {
+        return null;
+    }
+    // 2.
+    $oldFile = tempnam(sys_get_temp_dir(), 'ogg');
+    file_put_contents($oldFile, $oldData);
+    // 3.
+    $newFile = tempnam(sys_get_temp_dir(), 'mp3');
+    // 4.
+    shell_exec("ffmpeg -i " . escapeshellarg($oldFile) . " -f mp3 " . escapeshellarg($newFile));
+    // 5.
+    $newData = file_get_contents($newFile);
+
+    if ($newData === false) {
+        return null;
+    }
+    // 6.
+    $base64 = @base64_encode($newData);
+    // 7.
+    unlink($oldFile);
+    unlink($newFile);
+    return $base64;
+}
+
+function convert_base64_ogg_to_base64_wav(string $base64): ?string
+{
+    // 1.
+    $oldData = @base64_decode($base64);
+
+    if ($oldData === false) {
+        return null;
+    }
+    // 2.
+    $oldFile = tempnam(sys_get_temp_dir(), 'ogg');
+    file_put_contents($oldFile, $oldData);
+    // 3.
+    $newFile = tempnam(sys_get_temp_dir(), 'wav');
+    // 4.
+    shell_exec("ffmpeg -i " . escapeshellarg($oldFile) . " -acodec pcm_s16le -ac 2 -ar 44100 " . escapeshellarg($newFile));
+    // 5.
+    $newData = file_get_contents($newFile);
+
+    if ($newData === false) {
+        return null;
+    }
+    // 6.
+    $base64 = @base64_encode($newData);
+    // 7.
+    unlink($oldFile);
+    unlink($newFile);
+    return $base64;
+}
