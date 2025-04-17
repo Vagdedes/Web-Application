e7ec7e4ba9fbc1bb9ae6db6125596c357f8dd5dd
Evangelos <vaggdedes@gmail.com>
2025-04-17 22:08:36 +0300

    Update transactions.php

--- library/scheduler/tasks/transactions.php
+++ library/scheduler/tasks/transactions.php
@@ -2,6 +2,8 @@
 
 function transactions(int $pastDays): string
 {
+    require_once '/var/www/.structure/library/paypal/init.php';
+    require_once '/var/www/.structure/library/stripe/init.php';
     $bool = update_paypal_storage(0, $pastDays, true);
     $bool |= update_stripe_storage();
     $account = new Account();
