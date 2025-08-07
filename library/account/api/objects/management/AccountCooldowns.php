47047d4f4f66b19ece7a18177520cb0a2c9eee8e
Evangelos <vaggdedes@gmail.com>
2025-08-07 19:32:44 +0300

    Update AccountCooldowns.php

--- library/account/api/objects/management/AccountCooldowns.php
+++ library/account/api/objects/management/AccountCooldowns.php
@@ -14,7 +14,7 @@ class AccountCooldowns
         if ($this->account->exists()) {
             $action = string_to_integer($action);
 
-            if (!$this->has($action, false)) {
+            if (!$this->has($action, false, true, false)) {
                 sql_insert(AccountVariables::ACCOUNT_INSTANT_COOLDOWNS_TABLE,
                     array(
                         "account_id" => $this->account->getDetail("id"),
@@ -32,7 +32,7 @@ class AccountCooldowns
     {
         if ($this->account->exists()) {
             $action = string_to_integer($action);
-            $reply = $this->internalHas($action, false);
+            $reply = $this->internalHas($action, false, false);
 
             if ($reply->isPositiveOutcome()) {
                 $object = $reply->getObject();
@@ -65,47 +65,63 @@ class AccountCooldowns
         return false;
     }
 
-    private function internalHas(string $action, bool $hash = true): MethodReply
+    private function internalHas(
+        string $action,
+        bool   $hash = true,
+        bool   $instant = true,
+        bool   $buffer = true
+    ): MethodReply
     {
+        if (!$instant && !$buffer) {
+            return new MethodReply(false, "No cooldowns checked.");
+        }
         if (!$this->account->exists()) {
             return new MethodReply(false, "No account found.");
         }
         if ($hash) {
             $action = string_to_integer($action);
         }
-        if (!empty(get_sql_query(
-            AccountVariables::ACCOUNT_INSTANT_COOLDOWNS_TABLE,
-            array("id"),
-            array(
-                array("account_id", $this->account->getDetail("id")),
-                array("action_id", $action),
-                array("expiration", ">", time())
-            ),
-            null,
-            1
-        ))) {
+        if ($instant
+            && !empty(get_sql_query(
+                AccountVariables::ACCOUNT_INSTANT_COOLDOWNS_TABLE,
+                array("id"),
+                array(
+                    array("account_id", $this->account->getDetail("id")),
+                    array("action_id", $action),
+                    array("expiration", ">", time())
+                ),
+                null,
+                1
+            ))) {
             return new MethodReply(true);
         }
-        $query = get_sql_query(
-            AccountVariables::ACCOUNT_BUFFER_COOLDOWNS_TABLE,
-            array("id", "threshold"),
-            array(
-                array("account_id", $this->account->getDetail("id")),
-                array("action_id", $action),
-                array("expiration", ">", time())
-            ),
-            null,
-            1
-        );
+        if ($buffer) {
+            $query = get_sql_query(
+                AccountVariables::ACCOUNT_BUFFER_COOLDOWNS_TABLE,
+                array("id", "threshold"),
+                array(
+                    array("account_id", $this->account->getDetail("id")),
+                    array("action_id", $action),
+                    array("expiration", ">", time())
+                ),
+                null,
+                1
+            );
 
-        if (!empty($query)) {
-            return new MethodReply(true, null, $query[0]);
+            if (!empty($query)) {
+                return new MethodReply(true, null, $query[0]);
+            }
         }
         return new MethodReply(false);
     }
 
-    public function has(string $action, bool $hash = true): bool
+    public function has(
+        string $action,
+        bool   $hash = true,
+        bool   $instant = true,
+        bool   $buffer = true
+    ): bool
     {
-        return $this->internalHas($action, $hash)->isPositiveOutcome();
+        return $this->internalHas($action, $hash, $instant, $buffer)->isPositiveOutcome();
     }
 }
