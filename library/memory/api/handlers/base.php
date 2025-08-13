4bf12cc64d502f6ef1c7b06d4e0254d68668fd4c
Evangelos <vaggdedes@gmail.com>
2025-08-13 16:17:35 +0300

    Update base.php

--- library/memory/api/handlers/base.php
+++ library/memory/api/handlers/base.php
@@ -290,10 +290,6 @@ class IndividualMemoryBlock
 
     private function getBlock(bool $write = false): mixed
     {
-        if (!$write
-            && !@shmop_open($this->key, "a", 0, 0)) {
-            return false;
-        }
         return @shmop_open($this->key, $write ? "w" : "a", 0, 0);
     }
 
