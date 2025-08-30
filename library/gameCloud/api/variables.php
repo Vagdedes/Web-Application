84d9a2e687af22a37656a2a35338641813a1b602
Evangelos <vaggdedes@gmail.com>
2025-08-30 11:00:33 +0300

    Update variables.php

--- library/gameCloud/api/variables.php
+++ library/gameCloud/api/variables.php
@@ -6,7 +6,6 @@ class GameCloudVariables
         PURCHASES_TABLE = "gameCloud.purchases",
         LICENSE_MANAGEMENT_TABLE = "gameCloud.managedLicenses",
         IP_MANAGEMENT_TABLE = "gameCloud.managedIpAddresses",
-        PUNISHED_PLAYERS_TABLE = "gameCloud.punishedPlayers",
         CONNECTION_COUNT_TABLE = "gameCloud.connectionCount",
         ACCEPTED_PURPOSES_TABLE = "gameCloud.acceptedPurposes",
         FAILED_DISCORD_WEBHOOKS_TABLE = "gameCloud.failedDiscordWebhooks",
