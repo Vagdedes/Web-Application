d28182378b18b4326018a6d1a0532349cdecb96a
Evangelos <vaggdedes@gmail.com>
2026-02-13 19:29:24 +0200

    Update AbstractMethodReply.php

--- library/base/objects/AbstractMethodReply.php
+++ library/base/objects/AbstractMethodReply.php
@@ -3,19 +3,29 @@
 abstract class AbstractMethodReply
 {
 
-    private bool $success;
+    private ?bool $success;
 
-    public function __construct(bool $success)
+    public function __construct(?bool $success)
     {
         $this->success = $success;
     }
 
     public final function isPositiveOutcome(): bool
     {
-        return $this->success;
+        return $this->success === true;
     }
 
-    public final function setOutcome(bool $outcome): void
+    public final function isNegativeOutcome(): bool
+    {
+        return $this->success !== true;
+    }
+
+    public final function isStopOutcome(): bool
+    {
+        return $this->success === null;
+    }
+
+    public final function setOutcome(?bool $outcome): void
     {
         $this->success = $outcome;
     }
